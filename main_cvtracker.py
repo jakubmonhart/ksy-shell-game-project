15352757946388d5afcf5af05fe7b5986a580b3c
Ondin <kafka.ondrej@email.cz>
2022-01-11 13:32:10 +0100

    Added mains for sort and cv_tracker and yolo wrapper for getting BBs

---
+++ main_cvtracker.py
@@ -0,0 +1,99 @@
+import cv2
+import numpy as np
+import sys
+import argparse
+from yolov5.getBBs import yoloBBs 
+
+def run_tracking(video_path, tracker_type):
+
+  if tracker_type == "MOSSE":
+    create_tracker = cv2.legacy.TrackerMOSSE_create
+  elif tracker_type == "KCF":
+    create_tracker = cv2.TrackerKCF_create
+
+  video = cv2.VideoCapture("/home/ondin/Dokumenty/CTU/7_term/KSY/ksy-shell-game-project/data/high_normal.mp4")
+
+  if (video.isOpened()== False):
+    print("Error opening video file")
+    sys.exit()
+
+  # Read first frame.
+  ret, frame = video.read()
+  if ret==False:
+    print("Cannot read video file")
+    sys.exit()
+
+  bbGetter = yoloBBs()
+
+  bboxes = bbGetter.getBBs(frame)
+
+  # Select boxes
+  bboxes = [tuple(map(int, (x[0],x[1], (x[2]-x[0]), (x[3]-x[1])))) for x in bboxes]
+  colors = [(np.random.randint(0, 255), np.random.randint(0, 255), np.random.randint(0, 255)) for x in range(len(bboxes))]
+
+  print('Selected bounding boxes {}'.format(bboxes))
+
+  # Init trackers
+  trackers = []
+  for bbox in bboxes:
+    trackers.append(create_tracker())
+    trackers[-1].init(frame, bbox)
+
+  frame_skip=1
+  t = 0
+  while video.isOpened():
+    t+=1
+
+    # Read a new frame
+    ret, frame = video.read()
+    if ret==False:
+       break 
+
+    if (t%frame_skip != 0):
+      continue
+    
+    # Start timer
+    timer = cv2.getTickCount()
+
+    # Update trackers
+    boxes = []
+    tracking_failure = False
+    for tracker in trackers:
+      ret, box = tracker.update(frame)
+      
+      if ret==False:
+        tracking_failure = True
+
+      boxes.append(box)
+
+    # Calculate Frames per second (FPS)
+    fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer);
+
+    # Draw tracked objects
+    for i, box in enumerate(boxes):
+      p1 = (int(box[0]), int(box[1]))
+      p2 = (int(box[0] + box[2]), int(box[1] + box[3]))
+      cv2.rectangle(frame, p1, p2, colors[i], 2, 1)
+
+    if tracking_failure:
+      # Tracking failure
+      cv2.putText(frame, "Tracking failure detected", (100,80), cv2.FONT_HERSHEY_SIMPLEX, 0.75,(0,0,255),2)
+
+    # Display tracker type on frame
+    cv2.putText(frame, tracker_type + " Tracker", (100,20), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50),2);
+
+    # Display FPS on frame
+    cv2.putText(frame, "FPS : " + str(int(fps)), (100,50), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50), 2);
+
+    # Display result
+    cv2.imshow("MultiTracking", frame)
+
+    cv2.waitKey(1000)
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser()
+  parser.add_argument("--video_path", type=str ,default="data/video01.mp4")
+  parser.add_argument("--tracker_type", type=str ,default="KCF")
+
+  args = parser.parse_args()
+  run_tracking(args.video_path, args.tracker_type)
---
+++ yolov5/getBBs.py
@@ -0,0 +1,129 @@
+# YOLOv5 🚀 by Ultralytics, GPL-3.0 license
+
+import argparse
+import os
+import sys
+from pathlib import Path
+
+import cv2
+import torch
+import numpy as np
+import torch.backends.cudnn as cudnn
+
+FILE = Path(__file__).resolve()
+ROOT = FILE.parents[0]  # YOLOv5 root directory
+if str(ROOT) not in sys.path:
+    sys.path.append(str(ROOT))  # add ROOT to PATH
+ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
+
+from models.common import DetectMultiBackend
+from utils.datasets import IMG_FORMATS, VID_FORMATS, LoadImages, LoadStreams
+from utils.augmentations import Albumentations, augment_hsv, copy_paste, letterbox, mixup, random_perspective
+
+from utils.general import (LOGGER, check_file, check_img_size, check_imshow, check_requirements, colorstr,
+                           increment_path, non_max_suppression, print_args, scale_coords, strip_optimizer, xyxy2xywh)
+from utils.plots import Annotator, colors, save_one_box
+from utils.torch_utils import select_device, time_sync
+
+class yoloBBs:
+        # YOLOv5 get_bounding_boxes_from_numpyarrayedimage_wrapper_by_ondinka
+    @torch.no_grad()
+    def __init__(self, device='', half=False, max_det=1000,conf_thres=0.25, iou_thres=0.45, 
+                imgsz=(640, 640), agnostic_nms=False, weights=(ROOT / 'yolov5s.pt'),data=(ROOT / 'data/coco128.yaml'),dnn=False):
+        # Load model
+        self.device = select_device(device)
+        self.model = DetectMultiBackend(weights, device=self.device, dnn=dnn, data=data)
+        stride, names, pt, jit, onnx, engine = self.model.stride, self.model.names, self.model.pt, self.model.jit, self.model.onnx, self.model.engine
+        self.stride = stride
+        self.pt = pt
+        self.half = half
+        self.max_det = max_det
+        self.conf_thres = conf_thres
+        self.iou_thres = iou_thres
+        self.agnostic_nms = agnostic_nms
+        self.imgsz = check_img_size(imgsz, s=stride)  # check image size
+
+        # Half
+        self.half &= (self.pt or jit or engine) and self.device.type != 'cpu'  # half precision only supported by PyTorch on CUDA
+        if self.pt or jit:
+            self.model.model.half() if self.half else self.model.model.float()
+
+        bs = 1  # batch_size
+
+        # Run inference
+        self.model.warmup(imgsz=(1, 3, *imgsz), half=self.half)  # warmup
+
+    @torch.no_grad()
+    def getBBs(self, im, onlyBBs=True):
+        
+        in_shape = im.shape # input shape
+        # Padded resize
+        img = letterbox(im, self.imgsz, stride=self.stride, auto=self.pt)[0]
+
+        # Convert
+        img = img.transpose((2, 0, 1))[::-1]  # HWC to CHW, BGR to RGB
+        img = np.ascontiguousarray(img)
+
+        im = img
+        im = torch.from_numpy(im).to(self.device)
+        im = im.half() if self.half else im.float()  # uint8 to fp16/32
+        im /= 255  # 0 - 255 to 0.0 - 1.0
+        if len(im.shape) == 3:
+            im = im[None]  # expand for batch dim
+
+        # Inference
+        pred = self.model(im)
+
+        # NMS
+        pred = non_max_suppression(pred, self.conf_thres, self.iou_thres, None, self.agnostic_nms, max_det=self.max_det)
+
+        out_shape = im.shape # output shape
+        xrs = in_shape[1]/out_shape[3] #x resize factor
+        yrs = in_shape[0]/out_shape[2] # resize factor
+        pred = np.array([list(map(float, (bb[0]*xrs, bb[1]*yrs, bb[2]*xrs, bb[3]*yrs, bb[4]))) for bb in pred[0]])
+        return pred
+
+       
+
+def parse_opt():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--weights', nargs='+', type=str, default=ROOT / 'yolov5s.pt', help='model path(s)')
+    parser.add_argument('--source', type=str, default=ROOT / 'data/images', help='file/dir/URL/glob, 0 for webcam')
+    parser.add_argument('--data', type=str, default=ROOT / 'data/coco128.yaml', help='(optional) dataset.yaml path')
+    parser.add_argument('--imgsz', '--img', '--img-size', nargs='+', type=int, default=[640], help='inference size h,w')
+    parser.add_argument('--conf-thres', type=float, default=0.25, help='confidence threshold')
+    parser.add_argument('--iou-thres', type=float, default=0.45, help='NMS IoU threshold')
+    parser.add_argument('--max-det', type=int, default=1000, help='maximum detections per image')
+    parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+    parser.add_argument('--view-img', action='store_true', help='show results')
+    parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
+    parser.add_argument('--save-conf', action='store_true', help='save confidences in --save-txt labels')
+    parser.add_argument('--save-crop', action='store_true', help='save cropped prediction boxes')
+    parser.add_argument('--nosave', action='store_true', help='do not save images/videos')
+    parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --classes 0, or --classes 0 2 3')
+    parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
+    parser.add_argument('--augment', action='store_true', help='augmented inference')
+    parser.add_argument('--visualize', action='store_true', help='visualize features')
+    parser.add_argument('--update', action='store_true', help='update all models')
+    parser.add_argument('--project', default=ROOT / 'runs/detect', help='save results to project/name')
+    parser.add_argument('--name', default='exp', help='save results to project/name')
+    parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
+    parser.add_argument('--line-thickness', default=3, type=int, help='bounding box thickness (pixels)')
+    parser.add_argument('--hide-labels', default=False, action='store_true', help='hide labels')
+    parser.add_argument('--hide-conf', default=False, action='store_true', help='hide confidences')
+    parser.add_argument('--half', action='store_true', help='use FP16 half-precision inference')
+    parser.add_argument('--dnn', action='store_true', help='use OpenCV DNN for ONNX inference')
+    opt = parser.parse_args()
+    opt.imgsz *= 2 if len(opt.imgsz) == 1 else 1  # expand
+    print_args(FILE.stem, opt)
+    return opt
+
+
+def main(opt):
+    check_requirements(exclude=('tensorboard', 'thop'))
+    run(**vars(opt))
+
+
+if __name__ == "__main__":
+    opt = parse_opt()
+    main(opt)
